69ef38a3350497c7d5b876356196da964ec82ada
onagorodniuk <gagarinq9@gmail.com>
2020-03-10 19:53:25 +0200

    Feature/add alb support (#30)
    add elbv2
    
    Co-authored-by: Oleksandr Nagorodniuk <oleksandr.nagorodniuk@dgngames.com>
    Co-authored-by: elad <kaplan.elad@gmail.com>

---
+++ provider/aws/elbv2.go
@@ -0,0 +1,226 @@
+package aws
+
+import (
+	"encoding/json"
+	"finala/config"
+	"finala/expression"
+	"finala/storage"
+	"finala/structs"
+	"regexp"
+	"time"
+
+	awsClient "github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/cloudwatch"
+	"github.com/aws/aws-sdk-go/service/elbv2"
+	"github.com/aws/aws-sdk-go/service/pricing"
+	log "github.com/sirupsen/logrus"
+)
+
+// ELBV2ClientDescreptor is an interface defining the aws elbv2 client
+type ELBV2ClientDescreptor interface {
+	DescribeLoadBalancers(*elbv2.DescribeLoadBalancersInput) (*elbv2.DescribeLoadBalancersOutput, error)
+	DescribeTags(*elbv2.DescribeTagsInput) (*elbv2.DescribeTagsOutput, error)
+}
+
+// ELBV2Manager describe TODO::appname ELB struct
+type ELBV2Manager struct {
+	client           ELBV2ClientDescreptor
+	storage          storage.Storage
+	cloudWatchCLient *CloudwatchManager
+	pricingClient    *PricingManager
+	metrics          []config.MetricConfig
+	region           string
+
+	namespace          string
+	servicePricingCode string
+}
+
+// DetectedELBV2 define the detected AWS ELB instances
+type DetectedELBV2 struct {
+	Metric string
+	Region string
+	structs.BaseDetectedRaw
+}
+
+// TableName will set the table name to storage interface
+func (DetectedELBV2) TableName() string {
+	return "aws_elbv2"
+}
+
+// NewELBV2Manager implements AWS GO SDK
+func NewELBV2Manager(client ELBV2ClientDescreptor, st storage.Storage, cloudWatchCLient *CloudwatchManager, pricing *PricingManager, metrics []config.MetricConfig, region string) *ELBV2Manager {
+
+	st.AutoMigrate(&DetectedELBV2{})
+
+	return &ELBV2Manager{
+		client:           client,
+		storage:          st,
+		cloudWatchCLient: cloudWatchCLient,
+		metrics:          metrics,
+		pricingClient:    pricing,
+		region:           region,
+
+		namespace:          "AWS/ApplicationELB",
+		servicePricingCode: "AmazonEC2",
+	}
+}
+
+// Detect check with ELBV2 instance is under utilization
+func (r *ELBV2Manager) Detect() ([]DetectedELBV2, error) {
+	log.Info("Analyze ELBV2")
+	detectedELBV2 := []DetectedELBV2{}
+
+	instances, err := r.DescribeLoadbalancers()
+	if err != nil {
+		return detectedELBV2, err
+	}
+
+	now := time.Now()
+
+	for _, instance := range instances {
+
+		log.WithField("name", *instance.LoadBalancerName).Info("check ELBV2")
+
+		price, _ := r.pricingClient.GetPrice(r.GetPricingFilterInput(), "")
+
+		for _, metric := range r.metrics {
+
+			log.WithFields(log.Fields{
+				"name":        *instance.LoadBalancerName,
+				"metric_name": metric.Description,
+			}).Debug("check metric")
+
+			period := int64(metric.Period.Seconds())
+
+			metricEndTime := now.Add(time.Duration(-metric.StartTime))
+
+			regx, _ := regexp.Compile(".*loadbalancer/")
+
+			elbv2Name := regx.ReplaceAllString(*instance.LoadBalancerArn, "")
+
+			metricInput := cloudwatch.GetMetricStatisticsInput{
+				Namespace:  &r.namespace,
+				MetricName: &metric.Description,
+				Period:     &period,
+				StartTime:  &metricEndTime,
+				EndTime:    &now,
+				Dimensions: []*cloudwatch.Dimension{
+					&cloudwatch.Dimension{
+						Name:  awsClient.String("LoadBalancer"),
+						Value: &elbv2Name,
+					},
+				},
+			}
+
+			metricResponse, err := r.cloudWatchCLient.GetMetric(&metricInput, metric)
+
+			if err != nil {
+				log.WithError(err).WithFields(log.Fields{
+					"name":        *instance.LoadBalancerName,
+					"metric_name": metric.Description,
+				}).Error("Could not get cloudwatch metric data")
+				continue
+			}
+
+			instanceCreateTime := *instance.CreatedTime
+			durationRunningTime := now.Sub(instanceCreateTime)
+			totalPrice := price * durationRunningTime.Hours()
+
+			expression, err := expression.BoolExpression(metricResponse, metric.Constraint.Value, metric.Constraint.Operator)
+			if err != nil {
+				continue
+			}
+
+			if expression {
+
+				log.WithFields(log.Fields{
+					"metric_name":         metric.Description,
+					"Constraint_operator": metric.Constraint.Operator,
+					"Constraint_Value":    metric.Constraint.Value,
+					"metric_response":     metricResponse,
+					"name":                *instance.LoadBalancerName,
+					"region":              r.region,
+				}).Info("LoadBalancer detected as unutilized resource")
+
+				decodedTags := []byte{}
+				tags, err := r.client.DescribeTags(&elbv2.DescribeTagsInput{
+					ResourceArns: []*string{instance.LoadBalancerArn},
+				})
+				if err == nil {
+					decodedTags, err = json.Marshal(&tags.TagDescriptions)
+				}
+
+				elbv2 := DetectedELBV2{
+					Region: r.region,
+					Metric: metric.Description,
+					BaseDetectedRaw: structs.BaseDetectedRaw{
+						ResourceID:      *instance.LoadBalancerName,
+						LaunchTime:      *instance.CreatedTime,
+						PricePerHour:    price,
+						PricePerMonth:   price * 720,
+						TotalSpendPrice: totalPrice,
+						Tags:            string(decodedTags),
+					},
+				}
+				detectedELBV2 = append(detectedELBV2, elbv2)
+				r.storage.Create(&elbv2)
+
+			}
+
+		}
+	}
+
+	return detectedELBV2, nil
+
+}
+
+// GetPricingFilterInput prepare document elb pricing filter
+func (r *ELBV2Manager) GetPricingFilterInput() *pricing.GetProductsInput {
+
+	return &pricing.GetProductsInput{
+		ServiceCode: &r.servicePricingCode,
+		Filters: []*pricing.Filter{
+
+			&pricing.Filter{
+				Type:  awsClient.String("TERM_MATCH"),
+				Field: awsClient.String("usagetype"),
+				Value: awsClient.String("LoadBalancerUsage"),
+			},
+			&pricing.Filter{
+				Type:  awsClient.String("TERM_MATCH"),
+				Field: awsClient.String("productFamily"),
+				Value: awsClient.String("Load Balancer-Application"),
+			},
+			&pricing.Filter{
+				Type:  awsClient.String("TERM_MATCH"),
+				Field: awsClient.String("TermType"),
+				Value: awsClient.String("OnDemand"),
+			},
+
+			&pricing.Filter{
+				Type:  awsClient.String("TERM_MATCH"),
+				Field: awsClient.String("group"),
+				Value: awsClient.String("ELB:Balancer"),
+			},
+		},
+	}
+}
+
+// DescribeLoadbalancers return list of load loadbalancers
+func (r *ELBV2Manager) DescribeLoadbalancers() ([]*elbv2.LoadBalancer, error) {
+
+	input := &elbv2.DescribeLoadBalancersInput{}
+
+	resp, err := r.client.DescribeLoadBalancers(input)
+	if err != nil {
+		log.WithField("error", err).Error("could not describe elb instances")
+		return nil, err
+	}
+
+	instances := []*elbv2.LoadBalancer{}
+	for _, instance := range resp.LoadBalancers {
+		instances = append(instances, instance)
+	}
+
+	return instances, nil
+}
--- provider/aws/run.go
+++ provider/aws/run.go
@@ -14,6 +14,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/elasticache"
 	"github.com/aws/aws-sdk-go/service/elb"
+	"github.com/aws/aws-sdk-go/service/elbv2"
 	"github.com/aws/aws-sdk-go/service/lambda"
 	"github.com/aws/aws-sdk-go/service/pricing"
 	"github.com/aws/aws-sdk-go/service/rds"
@@ -60,6 +61,7 @@ func (app *Analyze) All() {
 			app.AnalyzeVolumes(app.storage, sess, pricing)
 			app.AnalyzeRDS(app.storage, sess, cloudWatchCLient, pricing)
 			app.AnalyzeELB(app.storage, sess, cloudWatchCLient, pricing)
+			app.AnalyzeELBV2(app.storage, sess, cloudWatchCLient, pricing)
 			app.AnalyzeElasticache(app.storage, sess, cloudWatchCLient, pricing)
 			app.AnalyzeLambda(app.storage, sess, cloudWatchCLient)
 			app.AnalyzeEC2Instances(app.storage, sess, cloudWatchCLient, pricing)
@@ -130,6 +132,48 @@ func (app *Analyze) AnalyzeELB(st storage.Storage, sess *session.Session, cloudW
 
 	elb := NewELBManager(elb.New(sess), st, cloudWatchCLient, pricing, metrics, *sess.Config.Region)
 	response, err := elb.Detect()
+
+	if err == nil {
+		b, _ := json.Marshal(response)
+		config := []structs.PrintTableConfig{
+			{Header: "ID", Key: "ResourceID"},
+			{Header: "Metric", Key: "Metric"},
+			{Header: "Region", Key: "Region"},
+			{Header: "Price Per Hour", Key: "PricePerHour"},
+			{Header: "Price Per Month", Key: "PricePerMonth"},
+		}
+		printers.Table(config, b, nil)
+		st.Create(&storage.ResourceStatus{
+			TableName: table.TableName(),
+			Status:    storage.Finish,
+		})
+	} else {
+		st.Create(&storage.ResourceStatus{
+			TableName:   table.TableName(),
+			Status:      storage.Error,
+			Description: err.Error(),
+		})
+	}
+
+	return err
+}
+
+// AnalyzeELBV2 will analyzes elastic load balancer resources
+func (app *Analyze) AnalyzeELBV2(st storage.Storage, sess *session.Session, cloudWatchCLient *CloudwatchManager, pricing *PricingManager) error {
+	metrics, found := app.metrics["elbv2"]
+	if !found {
+		return nil
+	}
+
+	table := &DetectedELBV2{}
+
+	st.Create(&storage.ResourceStatus{
+		TableName: table.TableName(),
+		Status:    storage.Fetch,
+	})
+
+	elbv2 := NewELBV2Manager(elbv2.New(sess), st, cloudWatchCLient, pricing, metrics, *sess.Config.Region)
+	response, err := elbv2.Detect()
 
 	if err == nil {
 		b, _ := json.Marshal(response)

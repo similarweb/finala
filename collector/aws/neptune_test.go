6d7f41d7bfdff267454595ff7e9d9b844aff230e
Costya Regev <costyar@similarweb.com>
2020-05-27 18:38:07 +0300

    Algin tests with the new neptune implementation

--- collector/aws/neptune_test.go
+++ collector/aws/neptune_test.go
@@ -2,9 +2,9 @@ package aws_test
 
 import (
 	"errors"
-	"finala/config"
-	"finala/provider/aws"
-	"finala/testutils"
+	"finala/collector/aws"
+	"finala/collector/config"
+	"finala/collector/testutils"
 	"testing"
 	"time"
 
@@ -40,8 +40,7 @@ func (np *MockAWSNeptuneClient) ListTagsForResource(*neptune.ListTagsForResource
 
 func TestDescribeNeptune(t *testing.T) {
 
-	mockStorage := testutils.NewMockStorage()
-
+	collector := testutils.NewMockCollector()
 	metrics := []config.MetricConfig{}
 
 	t.Run("valid", func(t *testing.T) {
@@ -50,12 +49,12 @@ func TestDescribeNeptune(t *testing.T) {
 			responseDescribeDBInstances: defaultNeptuneMock,
 		}
 
-		neptuneManager := aws.NewNeptuneManager(&mockClient, mockStorage, nil, nil, metrics, "us-east-1")
+		neptuneManager := aws.NewNeptuneManager(collector, &mockClient, nil, nil, metrics, "us-east-1")
 
 		result, _ := neptuneManager.DescribeInstances(nil, nil)
 
 		if len(result) != len(defaultNeptuneMock.DBInstances) {
-			t.Fatalf("unexpected neptune tables count, got %d expected %d", len(result), len(defaultNeptuneMock.DBInstances))
+			t.Fatalf("unexpected neptune instances count, got %d expected %d", len(result), len(defaultNeptuneMock.DBInstances))
 		}
 	})
 
@@ -66,7 +65,7 @@ func TestDescribeNeptune(t *testing.T) {
 			err:                         errors.New("error"),
 		}
 
-		neptuneManager := aws.NewNeptuneManager(&mockClient, mockStorage, nil, nil, metrics, "us-east-1")
+		neptuneManager := aws.NewNeptuneManager(collector, &mockClient, nil, nil, metrics, "us-east-1")
 
 		_, err := neptuneManager.DescribeInstances(nil, nil)
 
@@ -78,19 +77,20 @@ func TestDescribeNeptune(t *testing.T) {
 }
 
 func TestDetectNeptune(t *testing.T) {
-
-	mockStorage := testutils.NewMockStorage()
+	
+	collector := testutils.NewMockCollector()
 	mockCloudwatchClient := MockAWSCloudwatchClient{
 		responseMetricStatistics: defaultResponseMetricStatistics,
 	}
 	cloutwatchManager := aws.NewCloudWatchManager(&mockCloudwatchClient)
+
 	pricingManager := aws.NewPricingManager(&defaultPricingMock, "us-east-1")
 
 	mockClient := MockAWSNeptuneClient{
 		responseDescribeDBInstances: defaultNeptuneMock,
 	}
 
-	neptuneManager := aws.NewNeptuneManager(&mockClient, mockStorage, cloutwatchManager, pricingManager, defaultMetricConfig, "us-east-1")
+	neptuneManager := aws.NewNeptuneManager(collector,&mockClient, cloutwatchManager, pricingManager, defaultMetricConfig, "us-east-1")
 
 	response, _ := neptuneManager.Detect()
 
@@ -98,7 +98,45 @@ func TestDetectNeptune(t *testing.T) {
 		t.Fatalf("unexpected number of Neptune resources detected, got %d expected %d", len(response), 1)
 	}
 
-	if len(mockStorage.MockRaw) != 1 {
-		t.Fatalf("unexpected Neptune storage saved, got %d expected %d", len(mockStorage.MockRaw), 1)
+	if len(collector.Events) != 1 {
+		t.Fatalf("unexpected collector neptune resources, got %d expected %d", len(collector.Events), 0)
 	}
+
+	if len(collector.EventsCollectionStatus) != 2 {
+		t.Fatalf("unexpected resource status events count, got %d expected %d", len(collector.EventsCollectionStatus), 2)
+	}
+}
+
+
+func TestDetectNeptuneError(t *testing.T) {
+
+	collector := testutils.NewMockCollector()
+	pricingManager := aws.NewPricingManager(&defaultPricingMock, "us-east-1")
+
+	mockCloudwatchClient := MockAWSCloudwatchClient{
+		responseMetricStatistics: defaultResponseMetricStatistics,
+	}
+
+	cloutwatchManager := aws.NewCloudWatchManager(&mockCloudwatchClient)
+
+	mockClient := MockAWSNeptuneClient{
+		err: errors.New(""),
+	}
+
+	neptuneManager := aws.NewNeptuneManager(collector, &mockClient, cloutwatchManager, pricingManager, defaultMetricConfig, "us-east-1")
+
+	response, _ := neptuneManager.Detect()
+
+	if len(response) != 0 {
+		t.Fatalf("unexpected neptune databases detected, got %d expected %d", len(response), 0)
+	}
+
+	if len(collector.Events) != 0 {
+		t.Fatalf("unexpected collector neptune resources, got %d expected %d", len(collector.Events), 0)
+	}
+
+	if len(collector.EventsCollectionStatus) != 2 {
+		t.Fatalf("unexpected resource status events count, got %d expected %d", len(collector.EventsCollectionStatus), 2)
+	}
+
 }

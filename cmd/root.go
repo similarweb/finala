2e8f98562cb40e85bf6db18a3cfe9c632aac2819
Elad Kaplan <kaplan.elad@gmail.com>
2020-07-06 22:37:16 +0300

    Generate application version from github release by using goreleaser.

--- cmd/root.go
+++ cmd/root.go
@@ -16,8 +16,11 @@ var (
 	// cfgFile contine the path to the configuration file
 	cfgFile string
 
-	// Finala version
-	mainVersion = "0.3.3"
+	// version of the release, the value injected by .goreleaser
+	version = `{{.Version}}`
+
+	// commit hash of the release, the value injected by .goreleaser
+	commit = `{{.Commit}}`
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -26,21 +29,22 @@ var rootCmd = &cobra.Command{
 	Short: "Analyze wasteful and unused resources to cut unwanted expenses ",
 	Long: `A resource cloud scanner that analyzes and reports about wasteful and unused resources to cut unwanted expenses.
 The tool is based on yaml definitions (no code), by default configuration OR given yaml file and the report output will be saved in a given storage.`,
-	Version: mainVersion,
+	Version: getVersion(),
 }
 
 // Execute will expose all cobra commands
 func Execute() {
 
+	finalaVersion := getVersion()
 	params := &notifier.UpdaterParams{
 		Application: "finala",
-		Version:     mainVersion,
+		Version:     finalaVersion,
 	}
 
 	version, err := notifier.Get(params, notifier.RequestSetting{})
 
 	if err == nil && version.Outdated {
-		log.Error(fmt.Sprintf("newer Finala version available. latest version %s, current version %s, link download %s", version.CurrentVersion, mainVersion, version.CurrentDownloadURL))
+		log.Error(fmt.Sprintf("newer Finala version available. latest version %s, current version %s, link download %s", version.CurrentVersion, finalaVersion, version.CurrentDownloadURL))
 
 		for _, notification := range version.Notifications {
 			log.Error(notification.Message)
@@ -75,4 +79,9 @@ func initCmd() {
 		os.Exit(1)
 	}
 
+}
+
+// getVersion returns the current build version
+func getVersion() string {
+	return fmt.Sprintf("Finala %s (%s)", version, commit)
 }

380ed83210793c142133e4278f7d7a301b1f531e
Or Tzabary <or@tzabary.com>
2020-07-21 19:06:00 +0300

    getresourcetrends mock test

--- api/testutils/storage.go
+++ api/testutils/storage.go
@@ -88,6 +88,28 @@ func (ms *MockStorage) GetResources(resourceType string, executionID string, fil
 
 }
 
+func (ms *MockStorage) GetResourceTrends(resourceType string, filters map[string]string) ([]storage.ExecutionCost, error) {
+	var response []storage.ExecutionCost
+
+	if resourceType == "err" {
+		return nil, errors.New("error")
+	}
+
+	response = append(response, storage.ExecutionCost{
+		ExecutionID:        "dummy_123",
+		ExtractedTimestamp: 123,
+		CostSum:            16.5,
+	})
+	response = append(response, storage.ExecutionCost{
+		ExecutionID:        "dummy_312",
+		ExtractedTimestamp: 321,
+		CostSum:            19,
+	})
+
+	return response, nil
+
+}
+
 func (ms *MockStorage) GetExecutionTags(executionID string) (map[string][]string, error) {
 
 	response := map[string][]string{}
